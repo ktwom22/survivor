402c0e7f070c62f1b82ade1403e37f2be53e6585
ktwom22 <your-email@example.com>
2026-02-20 21:17:59 -0500

    player id

--- main.py
+++ main.py
@@ -212,8 +212,8 @@ def login():
 
 @app.route('/logout')
 def logout():
-    session.clear();
-    return redirect(url_for('global_leaderboard'))
+    session.clear()
+    return redirect(url_for('global_leaderboard')) # Use your actual landing page name
 
 
 # --- PASSWORD RECOVERY ---
